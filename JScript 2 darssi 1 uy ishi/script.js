1838b3f69895ab1d483d57530d18012f279ee8cf
sharofiddin1-abaskhanov <116502842+sharofiddin1-abaskhanov@users.noreply.github.com>
2022-10-30 00:12:49 +0500

    Add files via upload

---
+++ JScript 2 darssi 1 uy ishi/script.js
@@ -0,0 +1,40 @@
+
+
+
+// Homework 1
+
+var user1 = +prompt(' Yoshingizni kirirting! ')
+
+if( user1 > 0 && user1 <= 18 ) {
+    alert ( ' Siz hali yoshsiz, Oqishingiz kerak')
+}
+
+else if ( user1 > 18 && user1 <= 50) {
+    alert ( ' Siz ishlashingiz kerak!')
+}
+
+else if ( user1 > 50 && user1 <= 59) {
+    alert ( ' Siz yaqinda nafaqaga ketasiz!')
+}
+
+else if ( user1 > 59 && user1 <= 150) {
+    alert ( ' Siz nafaqaxursiz!')
+}
+
+else {
+    alert (' Nimadir notugri ketti ')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
